2c85c2aaf63f846383238a23bd43c33fb69370f1
1879LIN <mr.012607@outlook.com>
2024-03-17 18:55:07 +0000

    dd

--- hw4-multicycle/divider_unsigned_pipelined.sv
+++ hw4-multicycle/divider_unsigned_pipelined.sv
@@ -13,6 +13,75 @@ module divider_unsigned_pipelined (
 );
 
     // TODO: your code here
+    wire [31:0] dividend[33];
+    wire [31:0] remainder[33];
+    wire [31:0] quotient[33];
+     //register:
+    logic [31:0] reg_stage_2_dividend;
+    logic [31:0] reg_stage_2_divisor;
+    logic [31:0] reg_stage_2_remainder;
+    logic [31:0] reg_stage_2_quotient;
+
+    //stage 1:
+    assign dividend[0] = i_dividend;
+    assign remainder[0] = 32'b0;
+    assign quotient[0] = 32'b0;
+
+
+    genvar i;
+    genvar j;
+    
+    for (i = 0; i < 16; i = i + 1) begin  
+        divu_1iter u_divu_1iter(
+            .i_dividend  ( dividend[i]),
+            .i_divisor   ( i_divisor),
+            .i_remainder ( remainder[i]),
+            .i_quotient  ( quotient[i]),
+            .o_dividend  ( dividend[i+1]),
+            .o_remainder ( remainder[i+1]),
+            .o_quotient  ( quotient[i+1])
+        );
+    end
+
+
+   
+
+    always_ff @(posedge clk) 
+    if (rst) begin
+        reg_stage_2_dividend <= 32'b0;
+        reg_stage_2_divisor <= 32'b0;
+        reg_stage_2_remainder <= 32'b0;
+        reg_stage_2_quotient <= 32'b0;
+    end else begin
+        reg_stage_2_dividend <= dividend[16];
+        reg_stage_2_divisor <= i_divisor;
+        reg_stage_2_remainder <= remainder[16];
+        reg_stage_2_quotient <= quotient[16];
+    end
+
+    //stage 2:
+    assign dividend[16] = reg_stage_2_dividend;
+    assign remainder[16] = reg_stage_2_remainder;
+    assign quotient[16] = reg_stage_2_quotient;
+
+
+    
+
+    for (j = 16; j < 32; j = j + 1) begin 
+        divu_1iter u_divu_1iter(
+            .i_dividend  ( dividend[j]),
+            .i_divisor   ( reg_stage_2_divisor),
+            .i_remainder ( remainder[j]),
+            .i_quotient  ( quotient[j]),
+            .o_dividend  ( dividend[j+1]),
+            .o_remainder ( remainder[j+1]),
+            .o_quotient  ( quotient[j+1])
+        );
+    end
+
+
+    assign o_remainder = remainder[32];
+    assign o_quotient = quotient[32];
 
 
 endmodule
